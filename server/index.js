574f8c692c0c458fb3ff11a81b445b8f024dda8c
Phatphonnicha <151551494+bozung@users.noreply.github.com>
2024-03-11 11:49:09 +0700

    work 1

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 const express=require('express')
 const app=express()
 const port=8000
+ 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(express.static('public'))
+app.post('/api/form-post', (request, response) => {
+let name = request.body.name || ''
+let email = request.body.email || ''
+let msg = request.body.message || ''
 
- app.get('/', (req,res) => {
-    res.send(`
-    <!doctype html>
-    <html>
-    <head>
-    <title>Node.js</title>
-    </head>
-    <body>
-<h3>Welcome to Node.js</h3>
-<b>Node.js runs JavaScript on Server-Side</b>
- </body>
-</html>
-`)
+let text = `
+<table border="1">
+<caption>ข้อมูลที่ส่งขึ้นไป</caption>
+<tr><td>ชื่อ:</td><td>${name}</td></tr>
+<tr><td>ชื่อ:</td><td>${email}</td></tr>
+<tr><td>ชื่อ:</td><td>${msg}</td></tr>
+`
+response.send(text)
 })
-app.listen(port), ()=>console.log('Server listening on port '+port)
+app.listen(port, () => {
+console.log('Server listening on port ' + port)
+})
